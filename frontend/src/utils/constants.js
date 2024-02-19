bc69ddf6c79c370ad53d50d9c87b522d2ef492e3
Marina Tarasenko <zvezda_marina@mail.ru>
2024-02-19 20:13:46 +0300

    rm env

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "https://Localhost:4005";
+export const URL = "https://api.kpdmarina.nomoredomainsmonster.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
